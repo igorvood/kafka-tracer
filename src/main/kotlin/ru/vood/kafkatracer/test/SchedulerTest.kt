d5ef2e784a65552aa3fd598d8a1e842b36dc8b3f
vood <balitskY_igor@list.ru>
2022-12-08 12:07:14 +0300

    1

--- src/main/kotlin/ru/vood/kafkatracer/test/SchedulerTest.kt
+++ src/main/kotlin/ru/vood/kafkatracer/test/SchedulerTest.kt
@@ -13,14 +13,48 @@ class SchedulerTest(val userCache : UserCache) {
     private val logger: Logger = LoggerFactory.getLogger(SchedulerTest::class.java)
     @Scheduled(fixedDelay = 10000)
     fun sd(){
-    val joinToString =
+    val topicWithLastDate =
         userCache.userCache[TopicRequestGraphDto("dev_ivr__uasp_realtime__mdm_enrichment__for_additional_enrichment__uaspdto")]
             .messageKafka
-            .map { it.key + " " + Date(it.value.timestamp) }
-            .joinToString(", ")
-                logger.info("userCache size "+userCache.userCache.size())
-//        logger.info(joinToString)
+            .map { it.key to Date(it.value.timestamp) }
+            .toMap()
 
+
+        val maxOfLengthNameTopic = topicWithLastDate.keys.maxOf { it.length }
+
+        val maxOfLengthDate = topicWithLastDate.values.maxOf { it.toString().length }
+
+        val tableStr = topicWithLastDate
+            .map { tt ->
+
+                val date = tt.value.toString()
+                padRight(tt.key, maxOfLengthNameTopic - tt.key.length) to padRight(
+                    date,
+                    maxOfLengthDate - date.length
+                )
+
+            }.joinToString("\n") { "| ${it.first} | ${it.second} |" }
+
+
+
+        val totalStrLen = maxOfLengthNameTopic + maxOfLengthDate + 7
+        val repeat = "-".repeat(totalStrLen)
+
+        val s = "\n" + repeat + "\n" + tableStr + "\n" + repeat
+
+
+//            .joinToString(", ")
+//                logger.info("userCache size "+userCache.userCache.size())
+        logger.info(s)
+
+    }
+
+    fun padLeft(s: String, n: Int): String {
+        return "%${n}s".format(s)
+    }
+
+    fun padRight(s: String, n: Int): String {
+        return "%-${n}s".format(s)
     }
 
 }
