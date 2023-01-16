50e5240e19efe79634cc524b3f93dc86279cb898
vood <balitskY_igor@list.ru>
2023-01-16 11:43:20 +0300

    1

--- src/main/kotlin/ru/vood/kafkatracer/request/meta/cache/KafkaMessageListener.kt
+++ src/main/kotlin/ru/vood/kafkatracer/request/meta/cache/KafkaMessageListener.kt
@@ -24,7 +24,6 @@ class KafkaMessageListener(
 
         val pip = data.topic()
 
-
         val kafkaData = KafkaData(key, headers, timestamp, value, pip, identity)
 
         messageApplyFun(kafkaData)
