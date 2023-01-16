659f01921a97081a292d71e5c095b4f2e32896e1
vood <balitskY_igor@list.ru>
2023-01-16 10:21:10 +0300

    1

--- src/main/kotlin/ru/vood/kafkatracer/graph/abstraction/INode.kt
+++
@@ -1,7 +0,0 @@
-package ru.vood.kafkatracer.graph.abstraction
-
-interface INode {
-//    val nextNodes: Set<INode>
-
-//    val checkLimitationClassNextNodes: Set<KClass<out INode>>
-}
--- src/main/kotlin/ru/vood/kafkatracer/graph/abstraction/trace/asdasdasd.kt
+++
@@ -1,8 +0,0 @@
-package ru.vood.kafkatracer.graph.abstraction.trace
-
-fun main() {
-
-    val topic1 = Topic("asd", setOf())
-    val topic2 = Topic("asd", setOf(topic1))
-    topic2
-}
